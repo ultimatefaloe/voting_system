2166ac73250268608b30be8b66e7bc892f6f8c15
MSalaam20 <abdulsalammuhammad1234@gmail.com>
2026-04-18 12:22:01 +0100

    welcome to Voting Management System

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::inertia('/', 'welcome', [
     'canRegister' => Features::enabled(Features::registration()),
 ])->name('home');
 
+Route::inertia('/welcome', 'welcome/index')->name('welcome');
+
+Route::inertia('/privacy-policy', 'privacy-policy')->name('privacy-policy');
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
